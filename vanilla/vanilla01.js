c30bb4cd2e3c3760b7cb6b0786cbfb606e0d44b5
guddndhk <guddndhk@naver.com>
2022-04-19 00:32:47 +0900

    22/04/19 바닐라스트립트 스터디.

--- vanilla/vanilla01.js
+++ vanilla/vanilla01.js
@@ -12,3 +12,15 @@ console.log(typeof study2);
 console.log(typeof study3)//undefined
 
 console.log(typeof sutdy4, sutdy4);//object, null
+
+//es6 추가 문법 Symbol
+//유니크한 값을 만듬
+let testSymbol1 = Symbol(1);
+let testSymbol2 = Symbol(1);
+console.log(typeof testSymbol1, testSymbol1);
+console.log(typeof testSymbol2, testSymbol2);
+
+//== 같니? 라고 물어보는거고
+console.log(testSymbol1 == testSymbol2);//false
+// === 데이터 타입 까지도 똑같니? 라고 물어보는거다.
+console.log(testSymbol1 === testSymbol2);//false
