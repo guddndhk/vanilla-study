86e1ed2339233a920a3ae42f6422d5e8b38acfe2
guddndhk <guddndhk@naver.com>
2022-04-19 00:26:59 +0900

    Initial commit 22/04/19

---
+++ vanilla/vanilla01.js
@@ -0,0 +1,14 @@
+//변수 선언
+let study = "study";
+console.log(study);
+let study3;
+let sutdy4 = null;
+//상수
+const study2 = "study2";
+console.log(study2)
+
+console.log(typeof study2);
+
+console.log(typeof study3)//undefined
+
+console.log(typeof sutdy4, sutdy4);//object, null
